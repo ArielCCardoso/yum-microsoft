1988ba31e465925facbe0b738c487e97b6a8ad7f
Ariel C. Cardoso <arielccardoso@live.com>
2018-07-17 20:36:23 -0300

    Ajustes no Cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,9 @@
 # atributos
 default['yum']['microsoft']['repositoryid'] = 'microsoft'
-default['yum']['microsoft']['name'] = 'packages-microsoft-com-prod'
-default['yum']['microsoft']['description'] = "Pacotes Microsfot para #{node['platform_version'].to_i} - $basearch"
 case node['platform']
 when 'centos'
-  default['yum']['microsoft']['mirrorlist'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_i}/prod/"
+  default['yum']['microsoft']['baseurl'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_i}/prod/"
+  default['yum']['microsoft']['description'] = "Pacotes Microsoft para #{node['platform']} #{node['platform_version'].to_i}"
   # case "#{node['platform_version'].to_i}"
   # when '6'
   #  default['yum']['microsoft']['mirrorlist'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_i}/prod/"
@@ -12,7 +11,8 @@ when 'centos'
   #  default['yum']['microsoft']['mirrorlist'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_i}/prod/"
   # end
 when 'rhel'
-  default['yum']['microsoft']['mirrorlist'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_s}/prod/"
+    default['yum']['microsoft']['description'] = "Pacotes Microsfot para #{node['platform']} #{node['platform_version'].to_s}"
+    default['yum']['microsoft']['baseurl'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_s}/prod/"
     # case "#{node['platform_version'].to_s}"
     # when '6'
     #   default['yum']['microsoft']['mirrorlist'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_i}/prod/"
@@ -24,8 +24,8 @@ when 'rhel'
 #   default['yum']['microsoft']['gpgkey'] = "https://download.fedoraproject.org/pub/microsoft/RPM-GPG-KEY-microsoft-#{node['platform_version'].to_i}"
 end
 default['yum']['microsoft']['gpgkey'] = 'https://packages.microsoft.com/keys/microsoft.asc'
-default['yum']['microsoft']['failovermethod'] = 'priority'
-default['yum']['microsoft']['gpgcheck'] = true
-default['yum']['microsoft']['enabled'] = true
-default['yum']['microsoft']['managed'] = true
+#default['yum']['microsoft']['failovermethod'] = 'priority'
+#default['yum']['microsoft']['gpgcheck'] = 'True'
+#default['yum']['microsoft']['enabled'] = 'True'
+#default['yum']['microsoft']['managed'] = 'True'
 default['yum']['microsoft']['make_cache'] = true
