281b58610dcaf79eadf964e77d49659f8c33acc0
Ariel C. Cardoso <arielccardoso@live.com>
2018-07-18 15:14:01 -0300

    Ajustes

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,14 +11,14 @@ when 'centos'
   #  default['yum']['microsoft']['mirrorlist'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_i}/prod/"
   # end
 when 'rhel'
-    default['yum']['microsoft']['description'] = "Pacotes Microsfot para #{node['platform']} #{node['platform_version'].to_s}"
-    default['yum']['microsoft']['baseurl'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_s}/prod/"
-    # case "#{node['platform_version'].to_s}"
-    # when '6'
-    #   default['yum']['microsoft']['mirrorlist'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_i}/prod/"
-    # when '7'
-    #   default['yum']['microsoft']['mirrorlist'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_i}/prod/"
-    # end
+  default['yum']['microsoft']['description'] = "Pacotes Microsfot para #{node['platform']} #{node['platform_version'].to_s}"
+  default['yum']['microsoft']['baseurl'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_s}/prod/"
+  # case "#{node['platform_version'].to_s}"
+  # when '6'
+  #   default['yum']['microsoft']['mirrorlist'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_i}/prod/"
+  # when '7'
+  #   default['yum']['microsoft']['mirrorlist'] = "https://packages.microsoft.com/#{node['platform']}/#{node['platform_version'].to_i}/prod/"
+  # end
 # else
 #   default['yum']['microsoft']['mirrorlist'] = "http://mirrors.fedoraproject.org/mirrorlist?repo=microsoft-#{node['platform_version'].to_i}&arch=$basearch"
 #   default['yum']['microsoft']['gpgkey'] = "https://download.fedoraproject.org/pub/microsoft/RPM-GPG-KEY-microsoft-#{node['platform_version'].to_i}"
